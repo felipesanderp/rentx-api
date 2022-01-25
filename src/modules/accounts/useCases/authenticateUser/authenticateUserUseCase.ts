740433eed5cf154739cb5357690b20c17baeab4a
felipesanderp <felipe.panisson@hotmail.com>
2022-01-25 02:27:44 -0300

    create error middleware

--- src/modules/accounts/useCases/authenticateUser/authenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/authenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -28,13 +29,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Email or password incorret");
+      throw new AppError("Email or password incorret");
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error("Email or password incorret");
+      throw new AppError("Email or password incorret");
     }
 
     const token = sign({}, "98dc00c8e4599b10a95d816038b8bc35", {

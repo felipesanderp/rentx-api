00bb9b6b50a7d5d3bdc38332c781d6a5a132b2c6
Felipe Sander <felipe.panisson@hotmail.com>
2022-03-14 17:32:32 -0300

    create carSpecification

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -20,6 +20,8 @@ class CarsRepository implements ICarsRepository {
     license_plate,
     fine_amount,
     category_id,
+    specifications,
+    id
   }: ICreateCarDTO): Promise<Car> {
     const car = this.repository.create({
       name,
@@ -29,6 +31,8 @@ class CarsRepository implements ICarsRepository {
       license_plate,
       fine_amount,
       category_id,
+      specifications,
+      id
     });
 
     await this.repository.save(car);
@@ -62,6 +66,12 @@ class CarsRepository implements ICarsRepository {
     const cars = await carsQuery.getMany();
 
     return cars;
+  }
+
+  async findById(id: string): Promise<Car> {
+    const car = await this.repository.findOne(id);
+
+    return car;
   }
 }
 
--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -14,6 +14,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
     daily_rate,
     fine_amount,
     category_id,
+    id
   }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
@@ -25,6 +26,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
       daily_rate,
       fine_amount,
       category_id,
+      id
     });
 
     this.cars.push(car);
@@ -56,6 +58,12 @@ class CarsRepositoryInMemory implements ICarsRepository {
     });
 
     return all;
+  }
+
+  async findById(id: string): Promise<Car> {
+    const car = this.cars.find(car => car.id === id);
+
+    return car;
   }
 }
 

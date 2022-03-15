6021dc362a6cc3200226678fb7de985573ce82a3
Felipe Sander <felipe.panisson@hotmail.com>
2022-03-15 09:59:17 -0300

    add rentals useCase

---
+++ src/modules/rentals/useCases/createRental/CreateRentalController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+class CreateRentalController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { } = request.body;
+
+    const createRentalUseCase = container.resolve(CreateRentalUseCase);
+
+    // const rental = await createRentalUseCase.execute({
+    
+    // });
+
+    return response.status(201);
+  }
+}
+
+export { CreateRentalController };
